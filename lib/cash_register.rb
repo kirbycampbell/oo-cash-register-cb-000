e79e940529f37730894940df05586832be3e87a2
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-07 20:51:12 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,6 +2,7 @@
 class CashRegister
 attr_accessor :total, :discount
 
+# Makes .new happen / sets total to 0 / optionally sets discount
   def initialize(discount = nil)
     @total = 0
     @discount = discount
